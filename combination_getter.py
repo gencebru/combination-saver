f2a28bb6017c09193150ba6cdd00d8048fdf33a3
Ebru Genç <gencebru17@gmail.com>
2024-11-06 15:06:36 +0300

    add py files

---
+++ combination_getter.py
@@ -0,0 +1,18 @@
+def main():
+    get_combination([
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+        'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 
+        'v', 'w', 'x', 'y', 'z'
+    ])
+
+def get_combination(lst):
+    count = 2 ** len(lst)
+    for i in range(1, count):
+        binary_str = bin(i)[2:].zfill(len(lst))
+        for j in range(len(binary_str)):
+            if binary_str[j] == '1':
+                print(lst[j], end='')
+        print()
+
+if __name__ == "__main__":
+    main()
---
+++ get_combination_for_all.py
@@ -0,0 +1,28 @@
+def main():
+    combinations = get_combination([
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+        'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 
+        'v', 'w', 'x', 'y', 'z'
+    ])
+    
+    with open("combinations.txt", "w") as file:
+        for combo in combinations:
+            file.write(combo + "\n")
+    
+
+    print(f"Oluşturulan kombinasyon sayısı: {len(combinations)}")
+
+def get_combination(lst):
+    combinations = []
+    count = 2 ** len(lst)
+    for i in range(1, count):
+        binary_str = bin(i)[2:].zfill(len(lst))
+        combination = ""
+        for j in range(len(binary_str)):
+            if binary_str[j] == '1':
+                combination += lst[j]
+        combinations.append(combination)
+    return sorted(combinations)
+
+if __name__ == "__main__":
+    main()
